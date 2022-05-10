b707ca7e8c1452f986dacb9f78518b27fa9ebd40
ApluUalberta <aplu@ualberta.ca>
2022-05-10 13:44:35 -0600

    Untrack files in .gitignore

--- backend/src/resolvers/ReadingResolver.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from "type-graphql";
-import { Reading } from "../readings/reading.entitity
-@Resolver()
-export class ReadingResolver {
-  @Query(() => [Reading])
-  reading() {
-    return Reading.find;
-  }
-}
