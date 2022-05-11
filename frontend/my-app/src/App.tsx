4fb7e7266d75d8786f024ee66bd74c3c853a5545
ApluUalberta <aplu@ualberta.ca>
2022-05-10 23:53:15 -0600

    reinforced backend with more queries, dropdown menu created. Data needs to be formatted for visualization and proper dropdown!

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import LineChart from './components/LineChart';
 import {onError} from '@apollo/client/link/error';
-
+import Dropdown from './dropdown/Dropdown';
 
 export default function App() {
 
